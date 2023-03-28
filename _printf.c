35021f66e275a4a055023e41f90dece22b7d8004
abraham23o <abraonyonka23@gmail.com>
2023-03-28 20:46:32 +0300

    printf function

--- _printf.c
+++ _printf.c
@@ -1,51 +1,84 @@
 #include "main.h"
-
+#include <stdarg.h>
+#include <unistd.h>
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
+ * _putchar - writes a character to stdout
+ * @c: The character to print
  *
- * Return: number of chars printed.
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _printf - produces output according to a format.
+ * @format: A string containing zero or more directives
+ *
+ * Return: The number of characters printed
+ * (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	va_list args;
+	int i = 0, count = 0;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+	if (!format)
 		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+
+	va_start(args, format);
+	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
+			i++;
+
+			switch (format[i])
+			{
+				case 'c':
+				count += _putchar(va_arg(args, int));
+				break;
+
+				case 's':
+				count += _print_str(va_arg(args, char *));
+				break;
+
+				case '%':
+				count += _putchar('%');
+				break;
+
+				default:
+				count += _putchar('%');
+				count += _putchar(format[i]);
+				break;
 			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
 		}
 		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+	va_end(args);
+	return (count);
+}
+/**
+ * _print_str - prints a string
+ * @str: The string to print
+ *
+ * Return: The number of characters printed
+ */
+int _print_str(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		str = "(null)";
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
 }
